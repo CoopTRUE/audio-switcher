f9fefe517bd85cdc4cf9f72b23d1b953c136676e
CoopTRUE <coopdog4545@gmail.com>
2023-09-03 10:40:39 -0700

    Got audio switching working

---
+++ README.md
@@ -0,0 +1,17 @@
+# Audio Switcher
+
+Switches audio from speakers to headphones and vice versa.
+> Note: This script is only tested on Windows 11. It also downloads the [NirCmd](https://www.nirsoft.net/utils/nircmd.html) utility to switch audio devices.
+
+## Installation
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+```bash
+python main.py
+```
+
--- main.py
+++ main.py
@@ -1,16 +1,69 @@
-# This is a sample Python script.
+import sounddevice as sd
+import os
+import zipfile
+import requests
+import subprocess
+from tqdm import tqdm
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+TOGGLE = ["Headphones", "Speakers"]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def download_and_extract_nircmdc():
+    if os.path.exists("nircmdc.exe"):
+        return
+    response = requests.get("https://www.nirsoft.net/utils/nircmd-x64.zip", stream=True)
+    total_size = int(response.headers.get("content-length", 0))
+
+    with open("nircmd-x64.zip", "wb") as f, tqdm(
+        desc="Downloading nircmdc.exe",
+        total=total_size,
+        unit="B",
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for data in response.iter_content(chunk_size=1024):
+            bar.update(len(data))
+            f.write(data)
+
+    with zipfile.ZipFile("nircmd-x64.zip", "r") as zip_ref:
+        zip_ref.extract("nircmdc.exe")
+    os.remove("nircmd-x64.zip")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_devices():
+    devices = sd.query_devices()
+    return [
+        device
+        for device in devices
+        if device["max_output_channels"] > 0 and device["hostapi"] == 0
+    ]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def get_current_device_name():
+    devices = get_devices()
+    current_device_index = sd.default.device[1]
+    for device in devices:
+        if device["index"] == current_device_index:
+            return device["name"].split(" ")[0]
+    raise Exception("Current device not found")
+
+
+def get_other_device_name():
+    current_device_name = get_current_device_name()
+    return TOGGLE[0] if current_device_name == TOGGLE[1] else TOGGLE[1]
+
+
+def set_device(device_name):
+    subprocess.run(["nircmdc.exe", "setdefaultsounddevice", device_name])
+
+
+def toggle_device():
+    current_device_name = get_current_device_name()
+    other_device_name = get_other_device_name()
+    set_device(other_device_name)
+    print(f"[{current_device_name}] -> [{other_device_name}]")
+
+
+if __name__ == "__main__":
+    download_and_extract_nircmdc()
+    toggle_device()
